38b4af95461808b0c1df96a161011a5ba953aa47
okiochan <maryalx@mail.ru>
2018-01-12 17:42:30 +0300

    q

--- parzen/parzen.py
+++ parzen/parzen.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import classification_map as Map
 import LOO
 from sklearn import neighbors, datasets
+import sklearn
     
 def KNNski(x, r, X,Y, W='uniform'):
     # we create an instance of Neighbours Classifier and fit the data.
-    clf = neighbors.RadiusNeighborsClassifier(r, weights=W)
+    clf = neighbors.RadiusNeighborsClassifier(r, weights=W, metric='seuclidean')
     clf.fit(X, Y)
     x = np.atleast_2d(x)
     return clf.predict(x)
@@ -17,11 +18,11 @@ iris = datasets.load_iris()
 X = iris.data[:, [2,3]]
 Y = iris.target
 
-ks, err = LOO.LOO(X, Y, maxK=140, classifier = KNNski)
-bestR = ks[np.argmin(err)]
+# ks, err = LOO.LOO(X, Y, maxK=140, classifier = KNNski)
+# bestR = ks[np.argmin(err)]
 
-print("error for RadiusNeighborsClassifier")
-print( err[np.argmin(err)] / X.shape[0])
+# print("error for RadiusNeighborsClassifier")
+# print( err[np.argmin(err)] / X.shape[0])
 
 # plt.plot(ks, err)
 # plt.show()
